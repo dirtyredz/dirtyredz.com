11e9d5afd4455dc159a174dc47d88a928ea6dce7
dirtyredz <dirtyredz@live.com>
2018-07-28 08:15:59 -0500

    removed unused guery

--- src/pages/404.js
+++ src/pages/404.js
@@ -42,19 +42,5 @@ export const pageQuery = graphql`
         }
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "DD MMMM, YYYY")
-            title
-          }
-        }
-      }
-    }
   }
 `
--- src/pages/About.js
+++ src/pages/About.js
@@ -41,19 +41,5 @@ export const pageQuery = graphql`
         }
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "DD MMMM, YYYY")
-            title
-          }
-        }
-      }
-    }
   }
 `
