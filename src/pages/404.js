7ae2d21e86b61eb01dbab349c0c3fb976f89d5b5
dirtyredz <dirtyredz@live.com>
2018-09-27 12:16:02 -0500

    Centered 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import { Layout } from '../components'
+import { Layout, CenterContent } from '../components'
 
 class NotFoundPage extends React.Component {
   render() {
     const { data, location } = this.props
     return (
       <Layout data={data} location={location}>
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <br />
+        <br />
+        <CenterContent>
+          <h1>NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </CenterContent>
+        <br />
+        <br />
+        <br />
       </Layout>
     )
   }
